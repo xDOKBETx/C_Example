5c448ffb4476e6c81ba03381af39d201f6974676
Aleksey Valentinov <ds83351@chebnet.com>
2022-08-24 00:37:32 +0300

    Add folder Example003a_SumRandom

---
+++ Example003a_SumRandom/Program.cs
@@ -0,0 +1,9 @@
+﻿// Получение суммы двух чисел сгенерированных рандомно.
+
+int numberA = new Random().Next(1, 100);  // 1,2,3,4,...99
+Console.Write(numberA + " ");
+int numberB = new Random().Next(1, 100);
+Console.Write(numberB + " ");
+/*int result = numberA + numberB;  result -- промежуточная переменная
+Console.WriteLine(result); */
+Console.WriteLine(numberA + numberB);
