c42509467a73232e1d6169ac3ab97efbe8b3dae9
Aleksey Valentinov <ds83351@chebnet.com>
2022-08-24 00:19:33 +0300

    Add folder Example004_Div

---
+++ Example004_Div/Program.cs
@@ -0,0 +1,9 @@
+﻿// Получение частного двух чисел.
+
+/*int numberA = 10;
+int numberB = 5;
+*/
+double numberA = 12; // double -- позволяет работать с вещественными числами.
+double numberB = 5;
+
+Console.WriteLine(numberA / numberB);
