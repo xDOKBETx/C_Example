4559dbaae348f10fd8083f36b79ba08eef1c2b35
Aleksey Valentinov <ds83351@chebnet.com>
2022-08-24 01:18:43 +0300

    Add folder Example006_Base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,18 @@
+﻿// Решение задачи с гирями.
+
+int a = 1;
+int b = 9;
+int c = 3;
+int d = 11;
+int e = 6;
+
+int max = a;
+
+if(a > max) max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+if(d > max) max = d;
+if(e > max) max = e;
+
+Console.Write("max = " + max);
+
