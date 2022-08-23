2e4ad176f127424af11c8d66d0af89da106af7c9
Aleksey Valentinov <ds83351@chebnet.com>
2022-08-23 23:10:29 +0300

    Add folder Example001_HelloConsole

---
+++ Example001_HelloConsole/Program.cs
@@ -0,0 +1,4 @@
+﻿// Первое знакомство с C#
+Console.WriteLine("*Hello, World!!!*"); // Console - обращению к окну терминала
+Console.WriteLine("*Hello, World!!!*"); // WriteLine - вывод данных в окне терминала
+Console.WriteLine("*Hello, World!!!*");
