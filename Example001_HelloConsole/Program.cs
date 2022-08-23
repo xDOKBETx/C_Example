5b84c6efaf9f2ac41c8faa8d184196e435a14fe6
xDOKBETx <109816485+xDOKBETx@users.noreply.github.com>
2022-08-23 23:44:25 +0300

    Add folder Example001_HelloConsole

--- Example001_HelloConsole/Program.cs
+++ Example001_HelloConsole/Program.cs
@@ -1,4 +1,4 @@
 ﻿// Первое знакомство с C#
-Console.WriteLine("*Hello, World!!!*"); // Console - обращению к окну терминала.
-Console.WriteLine("*Hello, World!!!*"); // WriteLine - вывод данных с новой строки в окне терминала.
+Console.WriteLine("*Hello, World!!!*"); // Console -- обращению к окну терминала.
+Console.WriteLine("*Hello, World!!!*"); // WriteLine -- вывод данных с новой строки в окне терминала.
 Console.WriteLine("*Hello, World!!!*");
