41c61966e344a2d1f2086549314d87df9f8a6cd3
xDOKBETx <109816485+xDOKBETx@users.noreply.github.com>
2022-08-24 00:24:55 +0300

    Change

--- Example004_Diff/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// Получение частного двух чисел.
-
-/*int numberA = 10;
-int numberB = 5;
-*/
-double numberA = 12; // double -- позволяет работать с вещественными числами.
-double numberB = 5;
-
-Console.WriteLine(numberA / numberB);
