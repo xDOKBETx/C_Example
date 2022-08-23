cbf2f5dc8a8f29b2005c704a9b7687ba4057c56e
Aleksey Valentinov <ds83351@chebnet.com>
2022-08-24 00:41:23 +0300

    Add folder Example003a_SumRandom

--- Example003a_SumRandom/Program.cs
+++ Example003a_SumRandom/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Получение суммы двух чисел сгенерированных рандомно.
 
-int numberA = new Random().Next(1, 100);  // 1,2,3,4,...99
+int numberA = new Random().Next(1, 100);  // new Random().Next -- генерация случайных чисел в диапазоне от 1,2,3,4,...99
 Console.Write(numberA + " ");
 int numberB = new Random().Next(1, 100);
 Console.Write(numberB + " ");
