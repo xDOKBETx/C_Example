73f71de80348b563015f42d7f8e1fff2c7eac24e
Aleksey Valentinov <ds83351@chebnet.com>
2022-08-24 00:05:56 +0300

    Add folder Example003_Sum

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,5 @@
+﻿int numberA = 3;  // int -- integer -- целочисленные данные.
+int numberB = 5;
+/*int result = numberA + numberB;  result -- промежуточная переменная
+Console.WriteLine(result); */
+Console.WriteLine(numberA + numberB);
