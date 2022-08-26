f61717734d2617bdd588814ad8fb02bf843f7c54
Aleksey Valentinov <ds83351@chebnet.com>
2022-08-26 17:08:49 +0300

    Add Example013b_RecursionFactorial

---
+++ Example013b_RecursionFactorial/Program.cs
@@ -0,0 +1,13 @@
+﻿// Вычисление факториала через рекурсию.
+
+double Factorial(int n)
+{
+    // 1! = 1
+    // 0! = 1
+    if (n == 1) return 1; // Если n равно 1, то возвращаем 1
+    else return n * Factorial(n - 1);  // Иначе возвращаем n умноженное на факториал(n - 1)
+}
+    for (int i = 1; i < 40; i++)
+    {
+        Console.WriteLine($"{i}! = {Factorial(i)}"); 
+    }
