be5db6d7451dd9ded3800f77933f1f5150f0941a
Aleksey Valentinov <ds83351@chebnet.com>
2022-09-06 17:32:55 +0300

    Add Example014_LoopVsRecurcion

---
+++ Example014_LoopVsRecurcion/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Собрать строку с числами от меньшего к большему от a до b, a <= b
+*/
+
+string NumbersFor(int a, int b) // Строковая функция
+{
+    string result = String.Empty;  // Пустая строка
+    for (int i = a; i <= b; i++) // Для счётчика i присваиваем значение a; пока i меньше-равно b; увеличиваем значение счётчика на +1
+    {
+        result += $"{i} "; // Записываем в result значения счётчика + прибавляем к нему result(result = result + $"{i}")
+    }
+    return result; // Возвращаем result
+}
+
+string NumbersRec(int a, int b) // Строковая функция
+{
+    if (a <= b) return $"{a} " + NumbersRec(a + 1, b); // Если a меньше-равно b, 
+    // возвращаем значение a + вызываем функцию в которой увеличиваем значение первого аргумента a на 1 
+    else return String.Empty; // Иначе возвращаем пустую строку
+}
+
+Console.WriteLine(NumbersFor(1, 10));
+Console.WriteLine(NumbersRec(1, 10));
