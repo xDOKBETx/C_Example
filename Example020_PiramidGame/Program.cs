049dacd276ceaaf3a0951887b850ad1897272b37
Aleksey Valentinov <ds83351@chebnet.com>
2022-09-07 20:06:51 +0300

    Add Example020_PiramidGame

---
+++ Example020_PiramidGame/Program.cs
@@ -0,0 +1,10 @@
+﻿/* Игра в пирамидки
+*/
+
+void Towers(string with = "1", string on = "3", string some = "2", int count = 4) // 1 - откуда, 3 - куда, 2 - промежуточная; count - сколько блинов всего
+{
+    if (count > 1) Towers(with, some, on, count - 1);
+    Console.WriteLine($"{with} >> {on}");
+    if (count > 1) Towers(some, on, with, count - 1);
+}
+Towers();
